c83066c48dbc2da743366e19d53713b7f6480a7d
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-10-25 13:54:04 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/card/card";
+import Filter from "./components/filter/filter";
+import Header from "./components/header/header";
+import Movies from "./normalized-movies";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <main>
+        <Header />
+        <div className="container-xl">
+          <div className="row">
+            <div className="col-md-4 mb-4">
+              <div className="container">
+                <Filter />
+                <section className="mb-4">
+                  <h2>Bookmarked movies</h2>
+                  <ul className="bookmarked-movies list-group" />
+                </section>
+              </div>
+            </div>
+            <section className="col-md-8">
+              <h2 className="h5">Search results</h2>
+              <div className="alert alert-info">Use the form on the left to search for a movie</div>
+              <ul className="search-results row list-unstyled">
+                {Movies.map(movie => <Card {...movie} />)}
+              </ul>
+            </section>
+          </div>{/* row */}
+        </div>{/* container */}
+      </main>
+    </>
   );
 }
 
